4a6a0d334f5059bd3396c86e81c0dd006f8cb9d5
circlespainter <fabio@paralleluniverse.co>
2015-05-03 17:44:22 +0200

    Use 'ThreadLocalRandom'

--- src/main/java/quasarstocks/utils/FakeStockQuote.java
+++ src/main/java/quasarstocks/utils/FakeStockQuote.java
@@ -1,6 +1,6 @@
 package quasarstocks.utils;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Creates a randomly generated price based on the previous price
@@ -9,7 +9,7 @@ public class FakeStockQuote implements StockQuote {
 
     public Double newPrice(Double lastPrice) {
         // todo: this trends towards zero
-        return lastPrice * (0.95  + (0.1 * new Random().nextDouble())); // lastPrice * (0.95 to 1.05)
+        return lastPrice * (0.95  + (0.1 * ThreadLocalRandom.current().nextDouble())); // lastPrice * (0.95 to 1.05)
     }
 
 }
